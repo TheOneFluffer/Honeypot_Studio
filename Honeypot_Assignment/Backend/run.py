8e5a668b37c4e939dd255e8d3698b3db281a2b8c
TheOneFluffer <90469659+TheOneFluffer@users.noreply.github.com>
2024-11-18 00:25:47 -0800

    Update run.py

--- Honeypot_Assignment/Backend/run.py
+++ Honeypot_Assignment/Backend/run.py
@@ -1,6 +1,8 @@
 from app import create_app
+from app.api.honeypots import honeypots_bp
 
 app = create_app()
+app.register_blueprint(honeypots_bp, url_prefix='/api/honeypots')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
