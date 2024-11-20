efe212f81a197d63f26675f2ea20ba089a5db053
U-Ein2522 <WTWL25.22@ichat.sp.edu.sg>
2024-11-20 17:14:37 +0800

    Delete Honeypot_Assignment/Backend directory

--- Honeypot_Assignment/Backend/run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from app.api.honeypots import honeypots_bp
-
-app = create_app()
-app.register_blueprint(honeypots_bp, url_prefix='/api/honeypots')
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
