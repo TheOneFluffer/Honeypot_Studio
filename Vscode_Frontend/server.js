94dd39e29475bd30c6590ad3aba843f42cb7e08a
A-s-h-e-r-0 <awsomediver01@gmail.com>
2025-01-13 17:26:47 +0800

    Delete Vscode_Frontend directory

--- Vscode_Frontend/server.js
+++
@@ -1,8 +0,0 @@
-const app=require('./controller/app');
-
-var host="localhost";
-var port=8080;
-
-app.listen(port,host,function(){
-    console.log(`Server started at http://${host}:${port}`);
-});
