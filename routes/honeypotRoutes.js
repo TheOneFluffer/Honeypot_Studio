5d7b88d80fd291210f8da4b929b1bd70c5f3a674
GohJiaXin <135586980+GohJiaXin@users.noreply.github.com>
2025-01-13 21:47:11 +0800

    Update honeypotRoutes.js

--- routes/honeypotRoutes.js
+++ routes/honeypotRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getHoneypots, updateHoneypot, deployHoneypotController, removeHoneypotController, getHoneypotStatus,fetchLogs,fetchHoneypotLogs, checkForUploads,fetchAllLogs,getLogs, searchHoneypots, getContainerIDRoute } = require('../controllers/honeypotController');
+const {getHoneypots, updateHoneypot, deployHoneypotController, removeHoneypotController, getHoneypotStatus,fetchLogs,fetchHoneypotLogs, checkForUploads,fetchAllLogs,getLogs, searchHoneypots, getContainerIDRoute, startHoneypotController, stopHoneypotController } = require('../controllers/honeypotController');
 const router = express.Router();
 
 // Routes
@@ -16,4 +16,8 @@ router.get('/honeypots/alllogs/:containerId', fetchAllLogs); //Get all logs from
 router.get('/honeypots/logs',getLogs) //Get all filtered logs from Elkstack individually for every honeypot
 router.get('/honeypots/search',searchHoneypots) //Partial search of honeypots
 router.get('/honeypots/ContainerID', getContainerIDRoute) //Get container ID
+router.post('/honeypots/start/:name',startHoneypotController ) // get honeypot running
+router.post('/honeypots/stop/:name', stopHoneypotController) // stop honeypot running
+
+
 module.exports = router;
