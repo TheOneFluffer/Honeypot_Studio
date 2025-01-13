b43553b7e08539b65c2ebc5052b0d0c6e3f4df7c
GohJiaXin <135586980+GohJiaXin@users.noreply.github.com>
2025-01-13 21:50:27 +0800

    Create Honeypot.js

---
+++ models/Honeypot.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const honeypotSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    type: { type: String, required: true }, // e.g., 'low' or 'high' interaction
+    image: { type: String, required: true },
+    ports: { type: [Number], required: true },
+    IP: { type: String, required: true},
+    networkInterface:{type:String, required: true},
+    honeynet: {type: String, required: true},
+    deploymentLocation: {type: String, required: true},
+    logRetention: {type: Number, required: true},
+    alert:{type: String, required: true},
+    status: { type: String, default: 'active' }, // e.g., 'deployed', 'removed'
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+    
+});
+
+module.exports = mongoose.model('Honeypot', honeypotSchema);
