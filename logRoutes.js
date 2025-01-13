5062921039ced67e2a6dffd3515b02119bc3815b
TheOneFluffer <90469659+TheOneFluffer@users.noreply.github.com>
2025-01-13 17:31:48 +0800

    Delete logRoutes.js

--- logRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { collectAndInsertLogs } = require('../controllers/logController'); // Import the controller
-const router = express.Router();
-
-// Define the route for collecting logs from a specific honeypot
-router.get('/honeypots/:honeypotName/logs', collectAndInsertLogs);
-
-module.exports = router;
