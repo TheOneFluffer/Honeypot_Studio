53b4666694acaa2e971e8ab56bb3521df95e2eae
GohJiaXin <135586980+GohJiaXin@users.noreply.github.com>
2025-01-15 22:59:23 +0800

    Update honeypotRoutes.js

--- routes/honeypotRoutes.js
+++ routes/honeypotRoutes.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const {getHoneypots, updateHoneypot, deployHoneypotController, removeHoneypotController, getHoneypotStatus,fetchLogs,fetchHoneypotLogs, checkForUploads,fetchAllLogs,getLogs, searchHoneypots, getContainerIDRoute, startHoneypotController, stopHoneypotController } = require('../controllers/honeypotController');
+const {getHoneypots, deployHoneypotController, removeHoneypotController, getHoneypotStatus,fetchLogs,fetchHoneypotLogs, checkForUploads,fetchAllLogs,getLogs, searchHoneypots, getContainerIDRoute, startHoneypotController, stopHoneypotController, editHoneypotController } = require('../controllers/honeypotController');
 const router = express.Router();
 
 // Routes
 
 router.get('/honeypots', getHoneypots);
-router.put('/honeypots/:id', updateHoneypot);
+//router.put('/honeypots/:id', updateHoneypot);
 router.post('/honeypots/deploy', deployHoneypotController);
-router.delete('/honeypots/remove/:name/:containerID', removeHoneypotController);
+router.delete('/honeypots/remove/:name/:containerID', removeHoneypotController); //remove honeypot, elk stack, mongodb
 router.get('/honeypots/status', getHoneypotStatus);
-router.get('/logs/:containerId', fetchLogs);
+router.post('/logs/:containerId', fetchLogs); //Allows user to give criteria for severity of logs and grade logs according for the honeypots
 router.post('/honeypots/logs/:honeypotName', fetchHoneypotLogs);
-router.get('/honeypots/check-uploads/:containerID', checkForUploads);
+router.get('/honeypots/check-uploads/:containerID', checkForUploads); // check for file with .exe extension 
 router.get('/honeypots/alllogs/:containerId', fetchAllLogs); //Get all logs from docker individual honeypot 
 router.get('/honeypots/logs',getLogs) //Get all filtered logs from Elkstack individually for every honeypot
 router.get('/honeypots/search',searchHoneypots) //Partial search of honeypots
 router.get('/honeypots/ContainerID', getContainerIDRoute) //Get container ID
 router.post('/honeypots/start/:name',startHoneypotController ) // get honeypot running
 router.post('/honeypots/stop/:name', stopHoneypotController) // stop honeypot running
-
-
+router.put('/honeypots/edit', editHoneypotController); // editExistingHoneypot
 module.exports = router;
+
