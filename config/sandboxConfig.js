e6a6558a335a270b90616ac538c09f083162b4de
Shafeeq Amirudeen <103306939+Feeqy@users.noreply.github.com>
2025-01-15 12:13:18 +0800

    Added sandboxConfig.js

---
+++ config/sandboxConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+    VIRUSTOTAL_API_KEY: 'ac604e1ead3aa3dea15d2a6666c54bad43f46a307c1d5fb7f75b27fe744bd49d', // Replace with your actual VirusTotal API key
+    VIRUSTOTAL_API_URL: 'https://www.virustotal.com/api/v3/files', // File upload URL
+    TRIAGE_API_KEY: '3453495f22829ae74b3ce80c9095e2a69a7c051c', // Replace with your actual Triage API key
+    TRIAGE_API_URL: 'https://api.tria.ge/v0', // Base URL for Triage API
+};
