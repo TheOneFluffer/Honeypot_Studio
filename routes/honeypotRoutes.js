a12a103224a9cd55a919b0c0d489446a59fddf13
GohJiaXin <135586980+GohJiaXin@users.noreply.github.com>
2025-02-15 20:40:48 +0800

    Update honeypotRoutes.js

--- routes/honeypotRoutes.js
+++ routes/honeypotRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getHoneypots, deployHoneypotController, removeHoneypotController, getHoneypotStatus,fetchLogs,fetchHoneypotLogs, checkForUploads,fetchAllLogs,getLogs, searchHoneypots, getContainerIDRoute, startHoneypotController, stopHoneypotController, editHoneypotController } = require('../controllers/honeypotController');
+const {getHoneypots, deployHoneypotController, removeHoneypotController, getHoneypotStatus,fetchLogs,fetchHoneypotLogs, checkForUploads,fetchAllLogs,getLogs, searchHoneypots, getContainerIDRoute, startHoneypotController, stopHoneypotController, editHoneypotController, renameHoneypotController, getLogsHandler } = require('../controllers/honeypotController');
 const router = express.Router();
 
 // Routes
@@ -13,6 +13,8 @@ router.get('/logs/:containerId', fetchLogs); // Allow user to grade severity of 
 //router.post('/logs/:containerId', fetchLogs); //Allows user to give criteria for severity of logs and grade logs according for the honeypots
 router.post('/honeypots/logs/:honeypotName', fetchHoneypotLogs);
 router.get('/honeypots/check-uploads/:containerID', checkForUploads); // check for file with .exe extension 
+router.get('/honeypots/renamehoneypot', renameHoneypotController); //Rename Honeypot
+// router.post('/honeypots/renamehoneypot', renameHoneypotController); //Rename Honeypot
 router.get('/honeypots/alllogs/:containerId', fetchAllLogs); //Get all logs from docker individual honeypot 
 router.get('/honeypots/logs',getLogs) //Get all filtered logs from Elkstack individually for every honeypot
 router.get('/honeypots/search',searchHoneypots) //Partial search of honeypots
@@ -20,5 +22,6 @@ router.get('/honeypots/ContainerID', getContainerIDRoute) //Get container ID
 router.post('/honeypots/start/:name',startHoneypotController ) // get honeypot running
 router.post('/honeypots/stop/:name', stopHoneypotController) // stop honeypot running
 router.put('/honeypots/edit', editHoneypotController); // editExistingHoneypot
+router.get('/honeypots/checklogs',getLogsHandler) //Get all logs from all honeypot and filter according to keywords
 module.exports = router;
 
