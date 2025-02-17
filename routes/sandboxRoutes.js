5828e2fadf965975ffdb000faa5fe2561eaa23fd
Shafeeq Amirudeen <103306939+Feeqy@users.noreply.github.com>
2025-02-17 13:56:44 +0800

    Update sandboxRoutes.js

--- routes/sandboxRoutes.js
+++ routes/sandboxRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const { processLogsForExeFiles, triageprocessLogsForExeFiles} = require('../controllers/sandboxController');
+const { processLogsForExeFiles} = require('../controllers/sandboxController');
 const router = express.Router();
 
 // Route for transferring malware file to VirusTotal with analysis retrieval
 router.post('/sandbox/:honeypotName/processExeFiles', processLogsForExeFiles);
-
-// router.post('/sandbox/:honeypotName/traigeprocessExeFiles', triageprocessLogsForExeFiles);
 
 module.exports = router;
